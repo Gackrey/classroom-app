bc71c6acc19650a74df4c3814564b898bd58deb8
Gaurav <gauravdey625@gmail.com>
2021-01-12 14:58:44 +0530

    Trying to resolve Heroku issues

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const ejs = require('ejs');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const port = 3000||process.env.PORT;
+const port = process.env.PORT || 3000;
 require('./passport-setup')
 
 app.use(express.static(__dirname + '/static'));
 app.use(cors())
 app.use(bodyparser.urlencoded({ extended: false }))
-app.set('view-engine','ejs')
+app.set('view-engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyparser.json())
 app.use(cookieSession({
     name: 'tutosession',
@@ -20,9 +22,9 @@ app.use(cookieSession({
 
 const islogin = (req, res, next) => {
     if (req.user)
-    next()
+        next()
     else
-    res.sendStatus(401)
+        res.sendStatus(401)
 }
 app.use(passport.initialize());
 app.use(passport.session());
@@ -35,11 +37,12 @@ app.get('/failure', (req, res) => {
     res.send('Failed')
 })
 
-app.get('/success',islogin, (req, res) => {
+app.get('/success', islogin, (req, res) => {
     res.render('profile.ejs',
-    {name:req.user.displayName,
-    pic:req.user.photos[0].value
-    })
+        {
+            name: req.user.displayName,
+            pic: req.user.photos[0].value
+        })
 })
 
 app.get('/google',
@@ -52,15 +55,15 @@ app.get('/google',
 app.get('/google/callback',
     passport.authenticate('google', {
         failureRedirect: '/failure'
-    }),(req,res)=>{
+    }), (req, res) => {
         res.redirect('/success')
     });
 
-app.get('/logout',(req,res)=>{
+app.get('/logout', (req, res) => {
     req.session = null;
     req.logOut();
     res.redirect('/')
 })
 app.listen(port, () => {
-    console.log('Server started at port 3000');
+    console.log(`Server started at ${port}`);
 })
